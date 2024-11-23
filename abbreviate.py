b45109a48c8aa209835989fece6844a248ff6c01
Stan Stanmanson <91338907+ImSoBored420@users.noreply.github.com>
2024-11-22 20:46:41 -0500

    Add files via upload
    made this to abbreviate these trash things known as "states"; thank god this website link isn't case sensitive I would've been crying if it was

---
+++ abbreviate.py
@@ -0,0 +1,62 @@
+list = {
+"alaska":"AK",
+"alabama":"AL",
+"arkansas":"AR",
+"arizona":"AZ",
+"california":"CA",
+"colorado":"CO",
+"connecticut":"CT",
+"delaware":"DE",
+"florida":"FL",
+"georgia":"GA",
+"hawaii":"HI",
+"iowa":"IA",
+"idaho":"ID",
+"illinois":"IL",
+"indiana":"IN",
+"kansas":"KS",
+"kentucky":"KY",
+"louisiana":"LA",
+"massachusetts":"MA",
+"maryland":"MD",
+"maine":"ME",
+"michigan":"MI",
+"minnesota":"MN",
+"missouri":"MO",
+"mississippi":"MS",
+"montana":"MT",
+"north carolina":"NC",
+"north dakota":"ND",
+"nebraska":"NE",
+"new hampshire":"NH",
+"new jersey":"NJ",
+"new mexico":"NM",
+"nevada":"NV",
+"new york":"NY",
+"ohio":"OH",
+"oklahoma":"OK",
+"oregon":"OR",
+"pennsylvania":"PA",
+"rhode island":"RI",
+"south carolina":"SC",
+"south dakota":"SD",
+"tennessee":"TN",
+"texas":"TX",
+"utah":"UT",
+"virginia":"VA",
+"vermont":"VT",
+"washington":"WA",
+"Wisconsin":"WI",
+"west virginia":"WV",
+"wyoming":"WY",
+}
+def abbreviate_state(output):
+        abbreviate = list.get(f"{output}") 
+        return abbreviate
+if __name__ == "__main__":
+    def abbreviate_state(output):
+        abbreviate = list.get(f"{output}") 
+        return abbreviate
+if __name__ != "__main__":
+    print("kendricdjfdkxl")
+    
